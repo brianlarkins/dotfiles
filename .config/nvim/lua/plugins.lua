d7b2fadb53792397c1a79579160368f238c2afc4
Brian Larkins <larkinsb@rhodes.edu>
2024-09-23 14:23:04 -0500

    updated nvim config / completion, ARC SSH updates

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -154,15 +154,76 @@ require("lazy").setup({
   -- LSP / Neovim / Mason config
   {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
   {
+    "neovim/nvim-lspconfig",
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
     'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lua',
     'L3MON4D3/LuaSnip',
   },
 
+--  {'williamboman/mason.nvim',
+--    depeendencies = { 'williamboamn/mason-lspconfig.nvim', 'nvim-lua/plenary.nvim',},
+--    config = function()
+--      local lsp = require("lsp-zero")
+--      lsp.preset("recommended")
+--
+--      require('lsp-zero').setup()
+--      require('mason').setup()
+--
+--      require "mason-lspconfig".setup() {
+--        ensure_installed = {
+--          "bashls",
+--          "clangd",
+--          "cmake",
+--          "cssls",
+--          "dockerls",
+--          "gopls",
+--          "html",
+--          "jsonls",
+--          "pyright",
+--          "rust_analyzer",
+--          "sumneko_lua",
+--          "tsserver",
+--          "vimls",
+--          "yamlls",
+--        },
+--        auto_install = true,
+--      }
+--    end
+--  },
 
+  {"akinsho/toggleterm.nvim", version = "*", 
+    config = function()
+      require("toggleterm").setup {
+        size = 20,
+        open_mapping = [[<c-\>]],
+        hide_numbers = true, -- hide the number column in toggleterm buffers
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+        start_in_insert = true,
+        insert_mappings = true, -- whether or not the open mapping applies in insert mode
+        persist_size = true,
+        direction = 'horizontal',
+        close_on_exit = true, -- close the terminal window when the process exits
+        shell = vim.o.shell, -- change the default shell
+        float_opts = {
+          border = 'single',
+          width = 200,
+          height = 50,
+          winblend = 3,
+          highlights = {
+            border = "Normal",
+            background = "Normal",
+          }
+        }
+      }
+    end
+  },
 
   -- AI overlord
   {
